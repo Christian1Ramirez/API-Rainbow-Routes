9004317ee9b04e0b1b262fa090d651a1d3dfa481
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-08-26 11:49:22 -0700

    completed

--- build/index.js
+++ build/index.js
@@ -1,17 +1,14 @@
+"use strict";
 // Get the environment variables
-require("dotenv").config();
-
+require('dotenv').config();
 // Require needed node modules
-import express from "express";
-
+const express = require('express');
 // Initialize your application by calling the function returned by the express module
 const app = express();
-
 // Declare routes that people can visit on the application
-
 // White Page
-app.get("/", function (_req:express.Request, res:express.Response):void {
- res.send(`
+app.get('/', function (req, res) {
+    res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: white;">
                 <h2 style="text-align: center;">NAV BAR</h2>
@@ -20,12 +17,10 @@ app.get("/", function (_req:express.Request, res:express.Response):void {
         </body>
     `);
 });
-
 // Color Page
-app.get("/:color", function (req, res) {
- let myColor: unknown = req.params.color;
- if (typeof(myColor) === 'string') {
- res.send(`
+app.get('/:color', function (req, res) {
+    let myColor = req.params.color;
+    res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
                 <h2 style="text-align: center;">NAV BAR</h2>
@@ -33,8 +28,6 @@ app.get("/:color", function (req, res) {
             <h1 style="color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)} Page</h1>
         </body>
     `);
-    }
 });
-
 // Listen to a port number defined by a local environment variable
 app.listen(process.env.PORT);
